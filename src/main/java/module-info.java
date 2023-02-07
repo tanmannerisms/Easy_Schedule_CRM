7705c1ee54a53815e89160c94763129d7579686c
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-06 19:41:26 -0700

    Removed requirement from module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.easyschedule.wgu_c195 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.j;
 
 
     opens com.easyschedule to javafx.fxml;
