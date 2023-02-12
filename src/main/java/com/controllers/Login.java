651b726bb8ab364fd3c26122217e518d333669a1
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-12 13:54:24 -0700

    Removed the try catch statement from the LoginQuery.java methods. Added validations for username.

--- src/main/java/com/controllers/Login.java
+++ src/main/java/com/controllers/Login.java
@@ -4,18 +4,43 @@ import com.utils.LoginQuery;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.sql.SQLException;
+
 public class Login extends Controller{
-    private String username, password;
+    private String validUsername, validPassword, inputUsername, inputPassword;
     @FXML
     private TextField userNameField, passwordField;
     @FXML
-    private boolean validateCreds() {
-        username = userNameField.getText();
-        password = passwordField.getText();
-        if (password.equals(LoginQuery.getPassword(username))) {
-            openNotifyWindow("Password is correct");
-            return true;
+    private void validateCreds() {
+        inputUsername = userNameField.getText();
+        inputPassword = passwordField.getText();
+        if (validateUsername() && validatePassword()) {
+            openNotifyWindow("Creds are correct");
         }
-        return false;
+        else openNotifyWindow("Username or password incorrect");
+    }
+    private boolean validateUsername() {
+        try {
+            validUsername = LoginQuery.getUsername(inputUsername);
+            if (validUsername != null) {
+                if (validUsername.equals(inputUsername)) return true;
+            }
+            return false;
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+    private boolean validatePassword() {
+        try {
+            validPassword = LoginQuery.getPassword(inputUsername);
+            if (validPassword.equals(inputPassword)) return true;
+            else return false;
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
--- src/main/java/com/utils/LoginQuery.java
+++ src/main/java/com/utils/LoginQuery.java
@@ -5,32 +5,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class LoginQuery extends Query{
+    private static ResultSet resultSet;
+    private static PreparedStatement statement;
+    private static String sql;
 
-    public static String getPassword(String userName) {
-        ResultSet resultSet;
-        String sql = "SELECT Password FROM users WHERE User_Name = ?";
-        try {
-            PreparedStatement statement = JDBC.connection.prepareStatement(sql);
-            statement.setString(1, userName);
-            resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return resultSet.getString("Password");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+    public static String getPassword(String userName) throws SQLException{
+        sql = "SELECT Password FROM users WHERE User_Name = ?";
+        statement = JDBC.connection.prepareStatement(sql);
+        statement.setString(1, userName);
+        resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            return resultSet.getString("Password");
         }
-        return null;
+        else return null;
     }
-    public static String getUsername(String userName) {
-
-    }
-    public int insert() {
-        return 0;
-    }
-    public int update() {
-        return 0;
-    }
-    public int delete() {
-        return 0;
+    public static String getUsername(String userName) throws SQLException {
+        sql = "SELECT User_Name FROM users WHERE User_Name = ?";
+        statement = JDBC.connection.prepareStatement(sql);
+        statement.setString(1, userName);
+        resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            return resultSet.getString("User_Name");
+        }
+        else return null;
     }
 }
