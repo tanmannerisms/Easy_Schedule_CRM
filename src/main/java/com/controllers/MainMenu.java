057cbfe82e0cb680549e101e261aae6da6c304de
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-14 15:25:03 -0700

    Added a new main-menu.fxml and corresponding button functions.

--- src/main/java/com/controllers/MainMenu.java
+++ src/main/java/com/controllers/MainMenu.java
@@ -1,4 +1,15 @@
 package com.controllers;
 
+import javafx.fxml.FXML;
+
 public class MainMenu extends Controller {
+
+    @FXML
+    private void openCustomers() {
+
+    }
+    @FXML
+    private void openAppointments() {
+
+    }
 }
