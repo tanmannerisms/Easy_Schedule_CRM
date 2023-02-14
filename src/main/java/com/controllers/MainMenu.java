22a4dbbdc4e339ed0b2a8fbe5440b1f1910ead38
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-14 15:29:44 -0700

    Renamed openAppointments to openCalendar. Changed button and onAction attribute in fxml file to reflect.

--- src/main/java/com/controllers/MainMenu.java
+++ src/main/java/com/controllers/MainMenu.java
@@ -1,15 +1,18 @@
 package com.controllers;
 
+import com.window.Window;
 import javafx.fxml.FXML;
 
 public class MainMenu extends Controller {
 
     @FXML
     private void openCustomers() {
-
+        Window customers = new Window("customer-menu.fxml", "Customer Management");
+        customers.showWindowAndWait();
     }
     @FXML
-    private void openAppointments() {
-
+    private void openCalendar() {
+        Window calendar = new Window("calendar.fxml", "Calendar");
+        calendar.showWindowAndWait();
     }
 }
