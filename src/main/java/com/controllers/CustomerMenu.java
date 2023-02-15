a4736389213f48315be9a90995ea3f5c79cedb03
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-14 18:29:13 -0700

    Created the CustomerMenu controller and modified the fxml to reflect.

---
+++ src/main/java/com/controllers/CustomerMenu.java
@@ -0,0 +1,24 @@
+package com.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerMenu extends Controller implements Initializable {
+    @FXML
+    private TextField customerSearchField;
+    @FXML
+    private TableView customerTable;
+    @FXML
+    private TableColumn idColumn, nameColumn, addressColumn, divisionColumn, phoneNumberColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        populateTable();
+    }
+}
