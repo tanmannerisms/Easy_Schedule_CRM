e1e07f536fb573868b5e39d0ae5a1c577f74cffc
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-19 18:13:26 -0700

    Constructors, setters and getters in Country.java set.

--- src/main/java/com/location/Country.java
+++ src/main/java/com/location/Country.java
@@ -1,4 +1,27 @@
 package com.location;
 
 public class Country {
+    private int countryId;
+    private String countryName;
+
+    public Country(int id, String countryName) {
+        setCountryId(id);
+        setCountryName(countryName);
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
 }
