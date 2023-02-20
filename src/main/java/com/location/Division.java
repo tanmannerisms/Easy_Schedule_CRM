70deda7e85104782130903d910fd0f2d3c60f071
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-19 17:34:33 -0700

    Setters, getters and constructor for Division.java

--- src/main/java/com/location/Division.java
+++ src/main/java/com/location/Division.java
@@ -1,4 +1,35 @@
 package com.location;
 
 public class Division {
+    private int divisionId, countryId;
+    private String divisionName;
+    public Division(int divisionId, int countryId, String divisionName) {
+        setDivisionId(divisionId);
+        setCountryId(countryId);
+        setDivisionName(divisionName);
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
 }
