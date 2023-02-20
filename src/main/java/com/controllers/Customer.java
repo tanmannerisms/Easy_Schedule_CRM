8055cb12f36572932f9c01e5c88b62068900df13
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-19 17:27:54 -0700

    Initial setup of the controller class for customer.fxml

--- src/main/java/com/controllers/Customer.java
+++ src/main/java/com/controllers/Customer.java
@@ -1,4 +1,34 @@
 package com.controllers;
 
-public class Customer extends Controller{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Customer extends Controller implements Initializable {
+    private Customer customer;
+    @FXML
+    private TextField idField, nameField, addressField, postalCodeField, phoneNumberField;
+    @FXML
+    private ComboBox<Division> divisionSelector;
+    @FXML
+    private ComboBox<Country> countrySelector;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    private void onSaveClick(ActionEvent actionEvent) {
+
+    }
+    @FXML
+    private void setDivisions() {
+
+    }
 }
