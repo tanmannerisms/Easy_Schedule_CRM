a95065ebb48a1ef2e4c8f21f5f64a19c7c1db941
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-21 18:48:38 -0700

    Created Place abstract class.

---
+++ src/main/java/com/location/Place.java
@@ -0,0 +1,22 @@
+package com.location;
+
+public abstract class Place {
+    protected int id;
+    protected String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
