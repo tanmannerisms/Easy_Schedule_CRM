7131800fbc955f5c25dce5ba0d0161222829b3d5
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-22 06:01:33 -0700

    Loop in place for creating a string for updating database. Added a test method in main.

--- src/main/java/com/easyschedule/Main.java
+++ src/main/java/com/easyschedule/Main.java
@@ -1,6 +1,7 @@
 package com.easyschedule;
 
 import com.utils.JDBC;
+import com.utils.Query;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import com.window.*;
@@ -12,6 +13,8 @@ public class Main extends Application {
 
         Instance.updateAllLists();
 
+        test();
+
         Window login = new Window("login.fxml", "Login Page");
         login.showWindow();
 
@@ -19,5 +22,19 @@ public class Main extends Application {
     @Override
     public void stop() {
         JDBC.closeConnection();
+    }
+    public void test() {
+        long now = System.currentTimeMillis();
+        Query.update(
+                "cusotmers",
+                "Customer_Id = ",
+                "Customer_Name, Phone, Address, Postal_Code, Division_Id, Last_Update, Last_Updated_By",
+                "Tanner",
+                "541-571",
+                "83706",
+                "1",
+                String.valueOf(now),
+                "Me"
+        );
     }
 }
--- src/main/java/com/utils/Query.java
+++ src/main/java/com/utils/Query.java
@@ -63,13 +63,33 @@ public abstract class Query {
         }
     }
     public static boolean update(String table, String condition, String columns, String ... values) {
+        String columnArr[] = new String[values.length];
+        try {
+            StringBuilder column = new StringBuilder();
+            for (int j = 0; j < values.length; j++) {
+                for (int i = 0; i < columns.length(); i++) {
+                    if (columns.charAt(i) == ',') {
+                        column.append(" = ");
+                        columnArr[j] = String.valueOf(column);
+                        column = new StringBuilder();
+                    } else if (columns.charAt(i) == ' ') {
+                        // Do nothing
+                    } else {
+                        column.append(columns.charAt(i));
+                    }
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Number of columns does not match number of values.");
+        }
         String bindVariables = "?";
         if (values.length > 1) {
             for (int i = 0; i < values.length - 1; i++) {
                 bindVariables += ", ?";
             }
         }
-        sql = "UPDATE " + table + " (" + columns + ") VALUES (" + bindVariables + ")";
+        sql = "UPDATE " + table + " SET" ;
+/*
         try {
             statement = JDBC.connection.prepareStatement(sql);
             for (int i = 0; i < values.length; i++) {
@@ -92,6 +112,8 @@ public abstract class Query {
             System.out.println(e.getMessage());
             return false;
         }
+*/
+        return true;
     }
     public static boolean delete(String table, String condition, String comparison) {
         sql = "DELETE FROM " + table + " WHERE " + condition + " ?";
