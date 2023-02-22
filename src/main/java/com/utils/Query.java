2d5fbada65f40eae1b4a075827f449488a71e7b1
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-22 05:31:50 -0700

    Created copy of insert method to use as template for update.

--- src/main/java/com/utils/Query.java
+++ src/main/java/com/utils/Query.java
@@ -62,6 +62,37 @@ public abstract class Query {
             return false;
         }
     }
+    public static boolean update(String table, String condition, String columns, String ... values) {
+        String bindVariables = "?";
+        if (values.length > 1) {
+            for (int i = 0; i < values.length - 1; i++) {
+                bindVariables += ", ?";
+            }
+        }
+        sql = "UPDATE " + table + " (" + columns + ") VALUES (" + bindVariables + ")";
+        try {
+            statement = JDBC.connection.prepareStatement(sql);
+            for (int i = 0; i < values.length; i++) {
+                try {
+                    statement.setInt(i+1, Integer.parseInt(values[i]));
+                    continue;
+                } catch (NumberFormatException ignored){}
+                try {
+                    // Need to convert timestamp to UTC before updating DB!!!!!!
+                    Timestamp timestamp = new Timestamp(Long.valueOf(values[i]));
+                    statement.setTimestamp(i+1, timestamp);
+                    continue;
+                } catch (IllegalArgumentException ignored){}
+                statement.setString(i+1, values[i]);
+            }
+            statement.execute();
+            return true;
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
     public static boolean delete(String table, String condition, String comparison) {
         sql = "DELETE FROM " + table + " WHERE " + condition + " ?";
         try {
