3dbbe1a0a480b0c0fa6db0461b67c89539466924
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-23 12:05:57 -0700

    Controller Appointments.java created along with empty methods for buttons.

---
+++ src/main/java/com/controllers/Appointments.java
@@ -0,0 +1,22 @@
+package com.controllers;
+
+import javafx.fxml.FXML;
+
+public class Appointments extends Controller {
+    @FXML
+    private void  onSearchClick() {
+
+    }
+    @FXML
+    private void onAddClick() {
+
+    }
+    @FXML
+    private void onModifyClick() {
+
+    }
+    @FXML
+    private void onDeleteClick() {
+
+    }
+}
