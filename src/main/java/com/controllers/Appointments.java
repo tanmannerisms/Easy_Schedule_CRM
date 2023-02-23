0a6eeb0067f4cd7dac961bfb0e94d20e8b63697f
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-23 11:43:38 -1200

    Remade the fxml file to use the tabs as selectors instead of tabbed pages. Set up the updateTable function for it.

--- src/main/java/com/controllers/Appointments.java
+++ src/main/java/com/controllers/Appointments.java
@@ -4,21 +4,22 @@ import com.easyschedule.Appointment;
 import com.people.Customer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.sql.Date;
 import java.time.ZonedDateTime;
 import java.util.ResourceBundle;
 
 public class Appointments extends Controller implements Initializable {
     private Customer customer;
+    @FXML
+    private Label customerLabel;
     public TableColumn<Appointment, Integer> idColumn, customerColumn, userColumn;
     public TableColumn<Appointment, String> titleColumn, descriptionColumn, locationColumn, contactColumn, typeColumn;
-    public TableColumn<Appointment, ZonedDateTime>startColumn, endColumn;
+    public TableColumn<Appointment, Date> startColumn, endColumn;
     public TableView appointmentsTable;
+    public TabPane tabPane;
     public Tab allAppointments, monthAppointments, weekAppointments;
     public TextField appointmentSearchField;
 
@@ -28,6 +29,7 @@ public class Appointments extends Controller implements Initializable {
     }
     protected void setCustomer(Customer customer) {
         this.customer = customer;
+        customerLabel.setText("Customer: " + customer.getName());
     }
     @FXML
     private void  onSearchClick() {
@@ -44,5 +46,18 @@ public class Appointments extends Controller implements Initializable {
     @FXML
     private void onDeleteClick() {
 
+    }
+    @FXML
+    private void updateTable() {
+        Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
+        if (selectedTab.equals(allAppointments)) {
+            //Get all associated appointments
+        }
+        if (selectedTab.equals(monthAppointments)) {
+            // Get all associated appointments 31 days after today.
+        }
+        if (selectedTab.equals(weekAppointments)) {
+            // Get all associated appointments 7 days after today.
+        }
     }
 }
