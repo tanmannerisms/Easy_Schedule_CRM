c9d18bb2645eb6b4aac586f1003ede0f13901c4a
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-23 12:14:51 -0700

    Declared fxml Nodes.

--- src/main/java/com/controllers/Appointments.java
+++ src/main/java/com/controllers/Appointments.java
@@ -1,8 +1,23 @@
 package com.controllers;
 
+import com.easyschedule.Appointment;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoZonedDateTime;
 
 public class Appointments extends Controller {
+    public TableColumn<Appointment, Integer> idColumn, customerColumn, userColumn;
+    public TableColumn<Appointment, String> titleColumn, descriptionColumn, locationColumn, contactColumn, typeColumn;
+    public TableColumn<Appointment, ZonedDateTime>startColumn, endColumn;
+    public TableView appointmentsTable;
+    public Tab allAppointments, monthAppointments, weekAppointments;
+    public TextField appointmentSearchField;
+
     @FXML
     private void  onSearchClick() {
 
