6a3ee81e324717050fcdcfdbeb34c48f8a5b58c8
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-26 16:26:34 -0700

    Named the onAction methods.

--- src/main/java/com/controllers/ReportMenu.java
+++ src/main/java/com/controllers/ReportMenu.java
@@ -1,5 +1,7 @@
 package com.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -8,6 +10,18 @@ import java.util.ResourceBundle;
 public class ReportMenu extends Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    private void onAppointmentsClick(ActionEvent actionEvent) {
+
+    }
+    @FXML
+    private void onScheduleClick(ActionEvent actionEvent) {
+
+    }
+    @FXML
+    private void onFreeTimeClick(ActionEvent actionEvent) {
 
     }
 }
