c2a4ec13e84972f5cd71c60f4c3caee1a6a2a588
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-26 16:20:24 -0700

    Created the report-menu.fxml file and ReportMenu.java controller. Initial layout for report-menu.fxml set.

---
+++ src/main/java/com/controllers/ReportMenu.java
@@ -0,0 +1,13 @@
+package com.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportMenu extends Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
