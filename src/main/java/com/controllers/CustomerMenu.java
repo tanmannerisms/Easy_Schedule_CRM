94fc2954120699d625a6b3239ef03bb557a08eb6
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-25 18:07:34 -0700

    Successfully checking for associated appointments.

--- src/main/java/com/controllers/CustomerMenu.java
+++ src/main/java/com/controllers/CustomerMenu.java
@@ -78,7 +78,10 @@ public class CustomerMenu extends Controller implements Initializable {
     private void onDeleteClick(ActionEvent actionEvent) {
         Customer customer = getSelectedCustomer(actionEvent);
         if (customer != null){
-            if(!Instance.deleteCustomer(customer)) {
+            if (hasAssociatedAppointments(customer)){
+                openNotifyWindow("Please delete all apppointments for customer " + customer.getName() + " before deleting.", actionEvent);
+            }
+            else if(!Instance.deleteCustomer(customer)) {
                 openNotifyWindow("Deletion of customer with ID " + customer.getId() + "failed!", actionEvent);
             }
         }
@@ -101,5 +104,11 @@ public class CustomerMenu extends Controller implements Initializable {
         }
         actionEvent.consume();
         return selectedCustomer;
+    }
+    private boolean hasAssociatedAppointments(Customer customer) {
+        if (customer.getAssociatedAppointments().isEmpty()) {
+            return false;
+        }
+        else return true;
     }
 }
