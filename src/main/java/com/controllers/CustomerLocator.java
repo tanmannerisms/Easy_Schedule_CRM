e3ab08f3994d66b36805d26560cd9dde424d5586
Tanner Mills <tanner.mills2000@gmail.com>
2023-02-26 21:40:31 -0700

    Working so far. Need to test and convert objects to strings.

--- src/main/java/com/controllers/CustomerLocator.java
+++ src/main/java/com/controllers/CustomerLocator.java
@@ -2,6 +2,8 @@ package com.controllers;
 
 import com.easyschedule.Appointment;
 import com.easyschedule.Instance;
+import com.location.Country;
+import com.location.Division;
 import com.people.Customer;
 import com.utils.Query;
 import javafx.collections.FXCollections;
@@ -24,38 +26,25 @@ import java.time.ZonedDateTime;
 import java.util.ResourceBundle;
 
 public class CustomerLocator extends Controller implements Initializable {
-    private final ObservableList<Customer> CUSTOMERS = Instance.getAllCustomers();
-    private ObservableList<Appointment> tableAppointments = FXCollections.observableArrayList();
-    private ObservableList<Appointment> customerAppointments = FXCollections.observableArrayList();
-    private ObservableList<String> customerNames = FXCollections.observableArrayList();
-    private ObservableList<String> types = FXCollections.observableArrayList();
-    private ZonedDateTime now;
-    private Customer customer;
-    private Month month;
-    private String type;
+    private final ObservableList<Customer> ALLCUSTOMERS = Instance.getAllCustomers();
+    private ObservableList<Customer> tableCustomers = FXCollections.observableArrayList();
+    private ObservableList<Division> divisonList = FXCollections.observableArrayList();
     @FXML
     private TableView<Customer> customersTable;
     @FXML
-    private ComboBox<Month> divisionSelector;
+    private ComboBox<Division> divisionSelector;
     @FXML
-    private ComboBox<String> countrySelector;
+    private ComboBox<Country> countrySelector;
     @FXML
     private TableColumn<Customer, Integer> idColumn;
     @FXML
     private TableColumn<Customer, String> nameColumn, phoneColumn, addressColumn;
-    public CustomerLocator() {
-        // Set date to Jan 1 of current year
-        LocalDate today = LocalDate.now();
-        LocalDate january = LocalDate.ofYearDay(today.getYear(), 1);
-        LocalTime time = LocalTime.of(0,0);
-        now = ZonedDateTime.of(january,time,Instance.SYSTEMZONEID);
-    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        populateDivisions();
-        populateTypes();
-
         setTableColumns();
+        countrySelector.setItems(Instance.getAllCountries());
+        countrySelector.setValue(Instance.getCountry(1));
     }
     private void setTableColumns() {
         idColumn.setCellValueFactory(new PropertyValueFactory<>("Id"));
@@ -63,60 +52,36 @@ public class CustomerLocator extends Controller implements Initializable {
         phoneColumn.setCellValueFactory(new PropertyValueFactory<>("PhoneNumber"));
         addressColumn.setCellValueFactory(new PropertyValueFactory<>("Address"));
     }
-    private void populateTypes() {
-        try {
-            ResultSet rs = Query.selectUnique("type", "appointments");
-            while (rs.next()) {
-                types.add(rs.getString(1));
-            }
-        }
-        catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        countrySelector.setItems(types);
-    }
-    private void populateDivisions() {
-        ObservableList<Month> months = FXCollections.observableArrayList();
-        int year = now.getYear();
-        for (int i = 0; i < 12; i++) {
-            months.add(now.getMonth());
-            now = now.plusMonths(1);
-        }
-        now = now.withYear(year);
-        divisionSelector.setItems(months);
-    }
     @FXML
     private void selectCountry(ActionEvent actionEvent) {
-        type = null;
-        type = countrySelector.getValue();
-        if (divisionSelector.getValue() != null && customerSelector.getValue() != null) {
-            updateTable(actionEvent);
-            return;
+        divisonList.clear();
+        int selectedCountry = countrySelector.getValue().getId();
+        for (Division division : Instance.getAllDivisions()) {
+            if (division.getCountryId() == selectedCountry) {
+                divisonList.add(division);
+            }
         }
+        divisionSelector.setItems(divisonList);
         actionEvent.consume();
     }
+
     @FXML
     private void selectDivision(ActionEvent actionEvent) {
-        month = null;
-        month = divisionSelector.getValue();
-        if (countrySelector.getValue() != null && customerSelector.getValue() != null) {
+        if (countrySelector.getValue() != null && divisionSelector.getValue() != null) {
             updateTable(actionEvent);
             return;
         }
         actionEvent.consume();
     }
     private void updateTable(ActionEvent actionEvent) {
-        tableAppointments.clear();
-        for (Appointment appointment : customerAppointments) {
-            if (
-                    appointment.getType().equals(type) &&
-                            appointment.getStartDate().isAfter(now.withMonth(month.getValue())) &&
-                            appointment.getStartDate().isBefore(now.withMonth(month.getValue() + 1))
-            ) {
-                tableAppointments.add(appointment);
+        tableCustomers.clear();
+        int divId = divisionSelector.getValue().getId();
+        for (Customer customer : ALLCUSTOMERS) {
+            if (customer.getDivisionId() == divId) {
+                tableCustomers.add(customer);
             }
         }
-        customersTable.setItems(tableAppointments);
+        customersTable.setItems(tableCustomers);
         actionEvent.consume();
     }
 }
